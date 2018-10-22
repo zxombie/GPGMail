7f0e7a998f0257a478642b94a8e6993da02df51d
Lukas Pitschl <lukele@gpgtools.org>
2018-10-22 19:16:16 +0200

    Version: 2.8.1

--- Source/HeadersEditor+GPGMail.m
+++ Source/HeadersEditor+GPGMail.m
@@ -28,7 +28,7 @@
  */
 
 #import <Libmacgpg/Libmacgpg.h>
-#import <NSObject+LPDynamicIvars.h>
+#import "NSObject+LPDynamicIvars.h"
 #import "CCLog.h"
 #import "MFMailAccount.h"
 //#import "AddressAttachment.h"
