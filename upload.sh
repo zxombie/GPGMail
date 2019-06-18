ec314bb5829684c7142358ca614c4a9a88ceed7a
Andrew Turner <andrew@fubar.geek.nz>
2019-06-18 18:07:10 +0100

    Use the Cirrus-CI release script

--- upload.sh
+++ upload.sh
@@ -1,20 +1,25 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ -z "$GITHUB_TOKEN" ]; then
+if [[ "$CIRRUS_RELEASE" == "" ]]; then
+  echo "Not a release. No need to deploy!"
+  exit 0
+fi
+
+if [[ "$GITHUB_TOKEN" == "" ]]; then
   echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
   exit 1
 fi
 
 file_content_type="application/octet-stream"
 files_to_upload=(
-    gpgmail.tar.bz2
+  gpgmail.tar.bz2
 )
 
 for fpath in $files_to_upload
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_CHANGE_IN_REPO/assets?name=$name"
+  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
   curl -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
