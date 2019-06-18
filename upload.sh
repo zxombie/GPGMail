6c3d75ea0c094f8a30961a9f691002a42f2d045c
Andrew Turner <andrew@fubar.geek.nz>
2019-06-18 17:47:42 +0100

    Add an upload script

---
+++ upload.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "Please provide GitHub access token via GITHUB_TOKEN environment variable!"
+  exit 1
+fi
+
+file_content_type="application/octet-stream"
+files_to_upload=(
+    gpgmail.tar.xz
+)
+
+for fpath in $files_to_upload
+do
+  echo "Uploading $fpath..."
+  name=$(basename "$fpath")
+  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+  curl -X POST \
+    --data-binary @$fpath \
+    --header "Authorization: token $GITHUB_TOKEN" \
+    --header "Content-Type: $file_content_type" \
+    $url_to_upload
+done
