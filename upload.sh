e837c04efe15a30c89d5b2813d91003b75090d28
Andrew Turner <andrew@fubar.geek.nz>
2019-06-18 18:02:09 +0100

    Try using CIRRUS_CHANGE_IN_REPO for the relase name

--- upload.sh
+++ upload.sh
@@ -14,7 +14,7 @@ for fpath in $files_to_upload
 do
   echo "Uploading $fpath..."
   name=$(basename "$fpath")
-  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+  url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_CHANGE_IN_REPO/assets?name=$name"
   curl -X POST \
     --data-binary @$fpath \
     --header "Authorization: token $GITHUB_TOKEN" \
